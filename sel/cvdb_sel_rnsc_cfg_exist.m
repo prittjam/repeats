4d6ee71801bd1fa89c41c466a5ff0109d9031113
James Pritts <prittjam@cmp.felk.cvut.cz>
2011-01-21 15:18:55 +0100

    added cfg exist

---
+++ sel/cvdb_sel_rnsc_cfg_exist.m
@@ -0,0 +1,14 @@
+function [rnsc_cfg_exist, h] = cvdb_sel_rnsc_cfg_exist(conn, rnsc_cfg)
+    connh = conn.Handle;
+
+    h = cvdb_hash_rnsc_cfg(rnsc_cfg);
+    
+    
+    sql_statement = ['SELECT COUNT(*) FROM rnsc_cfgs WHERE id=' ...
+                     'UNHEX(?)'];
+    stm = connh.prepareStatement(sql_statement);
+    stm.setString(1, h);
+    rs = stm.executeQuery();
+    rs.next();
+    rnsc_cfg_exist = rs.getInt(1);
+
