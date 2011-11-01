40c8ddcb38ffbb746daea9d5eb44b1d317f29c46
James Pritts <jbpritts@gmail.com>
2011-11-01 10:32:26 +0100

    add init

---
+++ cvtk_init.m
@@ -0,0 +1,7 @@
+[pathstr, name, ext] = fileparts(mfilename('fullpath'));
+
+addpath([pathstr '/eg']);
+addpath([pathstr '/line']);
+addpath([pathstr '/pt']);
+addpath([pathstr '/rnsc']);
+addpath([pathstr '/tc']);
