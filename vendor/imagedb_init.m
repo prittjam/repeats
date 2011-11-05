8729ce2742f147d462ef85cc2931d3d52c1a7332
James Pritts <jbpritts@gmail.com>
2011-11-05 19:15:56 +0100

    removed init file

--- vendor/imagedb_init.m
+++
@@ -1,5 +0,0 @@
-function [] = imagedb_init()
-    [base_path, name, ext] = fileparts(mfilename('fullpath'));
-    
-    addpath([base_path '/matlab_interface']);
-    addpath([base_path '/matlab_interface/uniaccess']);
