dc1a8daa27db5be152cfc402fa88fa1faf2cb3c3
James Pritts <jbpritts@gmail.com>
2012-05-07 14:41:11 +0200

    added wireframe funcs

--- draw/draw3d_wireframe.m
+++ draw/draw3d_wireframe.m
@@ -1,1 +1,6 @@
-function [] = draw_wireframe_3d(vertices,edges)
+function [] = draw3d_wireframe(ax1,x1,edges)
+axes(ax1);
+hold on;
+plot(reshape(x1(1,edges),2,[]),reshape(x1(2,edges),2,[]),'y');
+plot(x1(1,:),x1(2,:),'b.');
+hold off;
