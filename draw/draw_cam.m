636d85329f668d2df4dad2aa32556cb0add9ddb4
James Pritts <jbpritts@gmail.com>
2012-05-07 13:06:40 +0200

    more functionality

--- draw/draw_cam.m
+++ draw/draw_cam.m
@@ -1,5 +1,12 @@
-function [] = draw_cam(ax1,P)
-[K R C] = Q2KRC(P);
-Beta = R';
-draw_csystem(ax1,Beta,C,'b','a');
+function [] = draw_cam(ax1,P,color,label)
+if nargin < 3
+    color = 'k';
+end
 
+if nargin < 4
+    label = '\beta';
+end
+
+[K R C] = cam_get_KRC_from_P(P);
+Beta = R';
+draw_affine_csystem(ax1,Beta,C,color,label);
