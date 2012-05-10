32368191ce2ecdf12f9c2d7dd1b2f8a3b72733c7
James Pritts <jbpritts@gmail.com>
2012-05-10 19:00:57 +0200

    change line_intersect.m

--- line/line_intersect.m
+++ line/line_intersect.m
@@ -1,14 +1,11 @@
 function pts = line_intersect(l1,l2)
-N = size(l1,2);
+n = size(l1,2);
 
-xn = l1(3,:).*l2(2,:)-l1(2,:).*l2(3,:);
-yn = l1(1,:).*l2(3,:)-l1(3,:).*l2(1,:);
+u = [l2(3,:).*l1(2,:)-l1(3,:).*l2(2,:); ...
+     -l1(1,:).*l2(3,:)+l1(3,:).*l2(1,:)];
+d = l1(1,:).*l2(2,:)-l1(2,:).*l2(1,:);
+ia = abs(d) > 1e-8;
 
-pts = zeros(2,N);
-
-d = 1./(l1(2,:).*l2(1,:)-l1(1,:).*l2(2,:));
-ia = abs(d) < 1e6;
-d = repmat(d(ia),[2 1]);
-
-pts(:,ia) = [xn(:,ia);yn(:,ia)].*d;
+pts = zeros(2,n);
+pts(:,ia) = bsxfun(@rdivide,u(:,ia),d(:,ia));
 pts(:,~ia) = nan;
