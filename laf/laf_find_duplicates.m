0800d5a6555d79658ae6878913048ad7ad4acf36
James Pritts <jbpritts@gmail.com>
2012-08-06 10:31:59 +0200

    add laf handling

---
+++ laf/laf_find_duplicates.m
@@ -0,0 +1,19 @@
+function [ic,clust_ind,n,k] = laf_find_duplicates(u,d,n)
+    abc = [1 2 4 5 7 8];
+    T = laf_agglom_clust(u(abc,:)',d);
+    h = hist(T,1:max(T));
+    ih = find(h < n);
+    [S,m] = setdiff(T,ih);
+    k = numel(S);
+    ia = find(ismember(T,S));
+    T = T(ia);
+    [TT,ib] = sort(T);
+    ic = ia(ib);
+    clust_ind = cell2mat(SplitVec(TT, 'equal', 'blockid'));
+    n = max(h);
+%    T(ib) = [col_ind{:}];
+
+function T = laf_agglom_clust(X,d)
+    Y = pdist(X,'euclidean');
+    Z = linkage(Y,'complete');
+    T = cluster(Z,'cutoff',d,'criterion','distance');
