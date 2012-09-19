0fc27f9fb2451dbd8b9194b56e8ec82844769b99
James Pritts <jbpritts@gmail.com>
2012-09-19 19:51:55 +0200

    more rnsc

---
+++ sampling/rnsc/rnsc_lo_make_est_Rt_from_np_cfg.m
@@ -0,0 +1,7 @@
+function cfg = rnsc_lo_make_est_Rt_from_np_cfg(cfg) 
+cfg.est_fn = @eg_est_Rt_from_np_lsqnonlin;
+cfg.min_trials = 20;
+cfg.max_trials = 20;
+cfg.max_data_retries = 1e2;
+cfg.confidence = 0.999;
+cfg = rnsc_standardize_cfg(cfg);
