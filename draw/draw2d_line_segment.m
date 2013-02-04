c32d4d596aeceecbbbda2a44489bb745f17a8737
James Pritts <jbpritts@gmail.com>
2013-02-04 17:28:28 +0100

    drawing functions

---
+++ draw/draw2d_line_segment.m
@@ -0,0 +1,16 @@
+function [hh,v] = draw2d_line_segment(ax1,u,l,varargin)
+v = line_project_points(ax1,u,l);
+draw2d_expand_axis(ax1,v);
+
+hold on;
+hh = plot(v(1,:)',v(2,:)',varargin{:});
+hold off;
+
+function v = line_project_points(ax1,u,l1)
+n = size(u,2);
+u = renormI(u);
+
+ln = l1./sqrt(sum(l1(1:2).^2));
+d = abs(sum(bsxfun(@times,ln,u)));
+v = ones(size(u));
+v(1:2,:) = u(1:2,:)-bsxfun(@times,d,ln(1:2,:));
---
+++ draw/draw3d_cam_array.m
@@ -0,0 +1,31 @@
+function [] = draw3d_cam_array(ax1,P,varargin)
+
+if nargin < 3
+    varargin = {};
+end
+
+p = inputParser;
+p.KeepUnmatched = true;
+p.addParamValue('edges',[]);
+p.addParamValue('edge_color','k');
+p.parse(varargin{:});
+
+for k = 1:numel(P);
+    if ~isempty(P{k})
+        draw3d_cam(ax1,P{k},varargin{:}, ...
+                   'CamLabel',sprintf('%d',k));
+    end
+end
+
+e = p.Results.edges;
+edge_color = p.Results.edge_color;
+
+hold on;
+for k = 1:size(e,2)
+    [~,~,c1] = cam_get_KRc_from_P(P{e(1,k)});
+    [~,~,c2] = cam_get_KRc_from_P(P{e(2,k)});
+    plot3([c1(1);c2(1)],[c1(2);c2(2)],[c1(3);c2(3)], ...
+          'Color',edge_color);
+end
+hold off;
+axis equal;
