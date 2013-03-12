a3e37cb5d0a34d632b3f9aa541a42497ea911005
James Pritts <jbpritts@gmail.com>
2013-03-12 14:11:07 +0100

    changes

--- hg/hg_est_H_from_4p.m
+++ hg/hg_est_H_from_4p.m
@@ -1,12 +1,9 @@
-function H = u2H(u,u0)
-if size(u,1) == 2
-    u = [u;ones(1,size(u,2))];
-    u0 = [u0;ones(1,size(u0,2))];
-end
+function H = hg_est_H_from_4p(ut,s,varargin)
+u = ut(:,s)';
+zero = zeros(sum(s),3);
 
-m = size(u,2);
-z = zeros(m,3);
-M = [u' z  bsxfun(@times,-u0(1,:),u)'; ...
-     z  u' bsxfun(@times,-u0(2,:),u)'];  
-[U S V] = svd(M);
-H = reshape(V(:,end),3,3)';
+A = [   zero    -u(:,1:3)   u(:,[5 5 5]).*u(:,1:3); ...
+      u(:,1:3)     zero    -u(:,[4 4 4]).*u(:,1:3) ];
+[U S V] = svd(A);
+
+H = { reshape(V(:,end),3,3)' };
