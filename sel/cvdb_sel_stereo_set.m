1a0dbb8739dbe6c95cf1c1640618bae86dd8c064
James Pritts <jbpritts@gmail.com>
2013-04-25 13:47:53 +0200

    fixed stereo set query

--- sel/cvdb_sel_stereo_set.m
+++ sel/cvdb_sel_stereo_set.m
@@ -11,43 +11,35 @@ function [stereo_set] = cvdb_sel_stereo_set(conn, set_name)
 
     if (count > 0)    
         img_num = 'img1_id';
-        sql_query_1 = ['SELECT url, imgs.name, ext, height, width, stereo_sets.id ' ...
-                       'FROM stereo_sets JOIN imgs ' ...
-                       'WHERE ' img_num  '=imgs.id ' ...
-                       'AND stereo_sets.name=?'];
-        
+
+        sql_query_1 = ['SELECT im1.url, im1.height, im1.width, ' ...
+                       'im2.url, im2.height, im2.width ' ...
+                       'FROM stereo_sets AS ss ' ...
+                       'INNER JOIN imgs AS im1 ON img1_id = im1.id ' ...
+                       'INNER JOIN imgs AS im2 ON img2_id = im2.id ' ...
+                       'WHERE ss.name = ?'];
+
         stm = connh.prepareStatement(sql_query_1);
         stm.setString(1, set_name);
         rs = stm.executeQuery();
 
-        img_num = 'img2_id';
-        sql_query_2 = ['SELECT url, imgs.name, ext, height, width ' ...
-                       'FROM stereo_sets JOIN imgs ' ...
-                       'WHERE ' img_num  '=imgs.id ' ...
-                       'AND stereo_sets.name=?'];
-        
-        stm = connh.prepareStatement(sql_query_2);
-        stm.setString(1, set_name);
-        rs2 = stm.executeQuery();
- 
         stereo_set = {};
         row_num = 0;
-        while (rs.next() && rs2.next())
+        while (rs.next())
             row_num = row_num+1;
 
             stereo_set(row_num).img1.url = ...
                 char(rs.getString(1));
             stereo_set(row_num).img1.height = ...
-                rs.getInt(4);
+                rs.getInt(2);
             stereo_set(row_num).img1.width = ...
-                rs.getInt(5);
-            stereo_set(row_num).id = rs.getString(6);
+                rs.getInt(3);
 
             stereo_set(row_num).img2.url = ...
-                char(rs2.getString(1));
+                char(rs.getString(4));
             stereo_set(row_num).img2.height = ...
-                rs2.getInt(4);
+                rs.getInt(5);
             stereo_set(row_num).img2.width = ...
-                rs2.getInt(5);
+                rs.getInt(6);
         end
     end
