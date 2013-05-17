03c6d9cf41d9c20009ad8f49507debd7f8c0833b
James Pritts <jbpritts@gmail.com>
2013-05-17 17:24:17 +0200

    added BMVC12 wrapper

---
+++ sampling/rnsc/rnsc_est_F_from_7p_bmvc12.m
@@ -0,0 +1,23 @@
+function varargout = rnsc_est_F_from_7p_bmvc12(u,s,sigma,confidence)
+
+cfg.tsq = 3.84*sigma^2;
+cfg.lo.tsq = cfg.tsq;
+
+tic;
+[Ft,inl,stats] = ransacF(u(:,s),cfg.tsq);
+res.time_elapsed = toc;
+
+res.solver = 'F-7p-BMVC12';
+res.tcCount = sum(s);
+res.model = Ft';
+res.weights = zeros(1,numel(inl));
+res.weights(inl) = 1;
+res.inliers_found = sum(res.weights);
+res.samples_drawn = stats(1);
+res.loCount = stats(2);
+
+varargout = { res };
+
+if nargout == 2
+    varargout = cat(2,varargout,cfg);
+end
