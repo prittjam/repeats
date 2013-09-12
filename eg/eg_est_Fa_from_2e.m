340994fc90a1e8ea4a76894e0b9a1d6d712a6b52
James Pritts <jbpritts@gmail.com>
2013-09-12 16:26:08 +0200

    added precalc for hg

--- eg/eg_est_Fa_from_2e.m
+++ eg/eg_est_Fa_from_2e.m
@@ -1,5 +1,5 @@
 function model_list = eg_est_Fa_from_2e(v)
-model_list = {};
+model_list = [];
 
 u = zeros(12,2);
 
@@ -12,15 +12,26 @@ CC2 = squeeze(mat2cell(zeros(3,3,2),3,3,ones(1,2)));
 ia = find(triu(ones(3)) == 1);
 
 CC1{1}(ia) = u(1:6,1);
-CC1{1} = CC1{1}+tril(CC1{1}.',-1);
+CC1{1} = CC1{1}+tril(CC1{1}',-1);
 CC1{2}(ia) = u(1:6,2);
-CC1{2} = CC1{2}+tril(CC1{2}.',-1);
+CC1{2} = CC1{2}+tril(CC1{2}',-1);
 
 CC2{1}(ia) = u(7:12,1);
-CC2{1} = CC2{1}+tril(CC2{1}.',-1);
+CC2{1} = CC2{1}+tril(CC2{1}',-1);
 CC2{2}(ia) = u(7:12,2);
-CC2{2} = CC2{2}+tril(CC2{2}.',-1);
+CC2{2} = CC2{2}+tril(CC2{2}',-1);
 
+%figure;
+%ax1 = subplot(1,2,1);
+%draw2d_lafs(ax1,v(1:9,:));
+%draw_ellipse(ax1,CC1{1});
+%draw_ellipse(ax1,CC1{2});
+%
+%ax2 = subplot(1,2,2);
+%draw2d_lafs(ax2,v(10:18,:));
+%draw_ellipse(ax2,CC2{1});
+%draw_ellipse(ax2,CC2{2});
+%
 [C21 C21_dual M1] = ell_canonicalize(CC1);
 [C22 C22_dual M2] = ell_canonicalize(CC2);
 
@@ -45,58 +56,66 @@ k4 = (p-p2)^2;
 
 C = [k4 k3 k2 k1 k0];
 
-if any(isnan(C))
-    return;
-end
+if isreal(C)
+    t = roots(C);
 
-if any(isinf(C))
-    return;
-end
+    ia = find(~imag(t));
 
-t = roots(C);
+    Fa_list = [];
 
-ia = find(~imag(t));
+    if (~isempty(ia))
+        cosa = 1./sqrt(1+t(ia).^2);
+        sina = sign(t(ia)).*sqrt(1-cosa.^2);
 
-Fa_list = [];
+        cosb = s*cosa;
 
-if (~isempty(ia))
-    cosa = 1./sqrt(1+t(ia).^2);
-    cosb = s*cosa;
-    sina = sign(t(ia)).*sqrt(1-cosa.^2);
-    sg_sinb = sign(((p-p2)*(1+t(ia).^2)-2*q*t(ia)+(r-p-(r2-p2)*s^2))./(2*s^2*q2));
-    sinb2 = -1*((p-p2)-2*q*sina.*cosa+(r-p-(r2-p2)*s^2).*cosa.^2)./(2*s*q2.*cosa);
-    sinb = sqrt(1-cosb.^2);
-    
-    Ft = zeros(3,3,length(cosa));
-    
-    Ft(3,2,:) = [cosa];
-    Ft(2,3,:) = [-cosb];
-    Ft(3,1,:) = [-sina];
-    Ft(1,3,:) = [sinb2];
-    invM1 = inv(M1);
-    invM2 = inv(M2);
+        sg_sinb = sign(((p-p2)*(1+t(ia).^2)-2*q*t(ia)+(r-p-(r2-p2)*s^2))./(2*s^2*q2));
+        sinb = sg_sinb.*sqrt(1-cosb.^2);
+        
+        Ft = zeros(3,3,length(cosa));
+        
+        Ft(3,2,:) = [cosa];
+        Ft(2,3,:) = [-cosb];
+        Ft(3,1,:) = [-sina];
+        Ft(1,3,:) = [sinb];
+        invM1 = inv(M1);
+        invM2 = inv(M2);
 
-    for i=1:size(Ft,3)
-        model_list{i} = invM2'*Ft(:,:,i)*invM1;
+        for i=1:size(Ft,3)
+            model_list{i} = invM2'*Ft(:,:,i)*invM1;
+        end
     end
-else
-    jjj = 3;
 end
 
 function [C2, C2_dual, M] = ell_canonicalize(CC1)
 M = calc_arandjelovic_faff_canonicalize(CC1{1}, CC1{2});
+C1 = M'*CC1{1}*M;
 C2 = M'*CC1{2}*M;
-T = mtx_make_T(ell_calc_center(C2));
+
+%figure;
+%hold on;
+%draw_ellipse(gca,C1);
+%draw_ellipse(gca,C2);
+%hold off;
+%kkk = 3;
+
+m2 = ell_calc_center(C2);
+T = mtx_make_T(m2(1:2));
 C2_dual = inv(T'*C2*T);
 
 function M = calc_arandjelovic_faff_canonicalize(C1,C2)
-A = ell_normalize(C1);
-C2n_cc = ell_calc_center(A'*C2*A);
-theta = atan2(C2n_cc(2),C2n_cc(1));
-if (sign(theta) < 0)
-    phi = -pi/2-theta;
-else
-    phi = pi/2-theta;
-end
-R = mtx_make_Rz(phi);
+A = ell_get_TRS_from_C(C1);
+
+%C1a = A'*C1*A;
+C2a = A'*C2*A;
+
+%figure;
+%hold on;
+%draw_ellipse(gca,C1a);
+%draw_ellipse(gca,C2a);
+%hold off;
+
+m2 = ell_calc_center(C2a);
+theta = atan2(m2(1),m2(2));
+R = mtx_make_Rz(theta);
 M = A*R;
--- ell/ell_get_TRS_from_C.m
+++ ell/ell_get_TRS_from_C.m
@@ -1,7 +1,12 @@
-function [T,R,S] = ell_get_tRS_from_C(C)
+function M = ell_get_TRS_from_C(C)
 m = ell_calc_center(C);
-[R,D] = eig(C(1:2,1:2));
 T = mtx_make_T(m(1:2));
-S0 = R'*T'*C*T*R;
-S00 = S0/S(3,3);
-S = [sqrt(1/abs(diag(S0)))];
+U = [chol(C(1:2,1:2))   zeros(2,1); ...
+     zeros(1,2)        1];
+M = T*inv(U);
+
+%U'*T'*C*T*invU;
+%/S0(3,3);
+%(sqrt(1./abs(diag(S00))));
+%T
+%
