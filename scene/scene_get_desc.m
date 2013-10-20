002c05043f7348bdbd4c1e33f7cb8ab2fe6f1cf1
James Pritts <jbpritts@gmail.com>
2013-10-20 23:11:42 +0200

    changes

---
+++ scene/scene_get_desc.m
@@ -0,0 +1,14 @@
+function [res,is_found] = scene_get_desc(img_id,descriptors)
+global conn
+
+res = cell(1,numel(descriptors));
+is_found = false(1,numel(descriptors));
+
+for k = 1:numel(res)
+    if strcmp(descriptors(k).readcache,'On')
+        [res{k},isf] = cvdb_sel_desc(conn,img_id,descriptors(k).key);
+        is_found(k) = isf;
+    end
+end
+
+is_found = logical(is_found);
---
+++ scene/scene_put_desc.m
@@ -0,0 +1,8 @@
+function [] = scene_put_desc(img_id,desc,res)
+global conn
+
+for k = 1:numel(res)
+    if strcmp(desc(k).writecache,'On')
+        cvdb_ins_desc(conn,img_id,desc(k).key,res{k});
+    end
+end
