3e1e95e0c28e451a4cc88e7b8a8744e84b8324ac
James Pritts <prittjam@lcnode1.felk.cvut.cz>
2014-10-28 19:33:42 +0100

    remove stuff

--- ins/cvdb_ins_desc.m
+++
@@ -1,8 +0,0 @@
-function [] =  cvdb_ins_desc(conn,img_id,key,desc)
-try
-    if ~isempty(desc)
-        conn.imagedb.storage.put(img_id,desc,['descriptor:' key],[]); 
-    end
-catch err
-    disp(err.message);
-end
