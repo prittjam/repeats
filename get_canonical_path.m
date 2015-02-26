75fa7a404fcf5687d94340a5fff1b1003737c3a8
James Pritts <prittjam@cmp.felk.cvut.cz>
2015-02-26 17:39:45 +0100

    changes

--- get_canonical_path.m
+++ get_canonical_path.m
@@ -1,2 +1,7 @@
-function oPath = get_canonical_path(iPath)
-oPath = cd(cd(iPath));
+function ourl = get_canonical_path(iurl)
+[ipath,name,ext] = fileparts(iurl)
+if isempty(ipath)
+    ipath = pwd;
+end
+opath = cd(cd(ipath));
+ourl = [opath '/' name ext];
