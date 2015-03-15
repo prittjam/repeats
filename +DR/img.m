c2f35b34ae5447660c0533f53c9cde56d08ba6be
James Pritts <prittjam@cmp.felk.cvut.cz>
2015-03-15 16:57:15 +0100

    changes

--- +DR/img.m
+++ +DR/img.m
@@ -10,22 +10,32 @@ classdef img < handle
     end
 
     methods(Access=public)
-        function this = img(data,varargin)
-            this.data = data;
-            this.height = size(data,1);
-            this.width = size(data,2);
+        function this = img(varargin)
+            cfg = struct('data',[],'url',[],'cid',[]);
+            cfg = helpers.vl_argparse(cfg,varargin{:});
+            this.data = cfg.data;
+            this.cid = cfg.cid;
+           
+            if ~isempty(cfg.url)
+                this.url = fullfile(cfg.url);
+            end
+            
+
+            if isempty(this.data) && ~isempty(this.url)
+                this.data = imread(this.url);
+                filecontents = get_raw_img(this.url);
+                this.cid = HASH.hash(filecontents,'MD5');
+            end
+
+            this.height = size(this.data,1);
+            this.width = size(this.data,2);
+            this.area = this.width*this.height;
 
             if (ndims(this.data) == 3)
-                this.intensity = rgb2gray(data);
-            else
+                this.intensity = rgb2gray(this.data);
+            elseif (ndims(this.data) == 1)
                 this.intensity = data;
             end
-            this.area = this.width*this.height;
-            
-            cfg = struct('url',[],'cid',[]);
-            cfg = helpers.vl_argparse(cfg,varargin{:});
-            this.url = cfg.url;
-            this.cid = cfg.cid;
         end
     end
     
