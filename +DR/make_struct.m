c8fc2aa03e7310874c3e02453d3c517d3e81ab3f
James Pritts <prittjam@cmp.felk.cvut.cz>
2015-07-23 17:36:20 +0200

    changes

--- +DR/make_struct.m
+++
@@ -1,9 +0,0 @@
-function scfg = make_struct(cfg)
-    warning('off','MATLAB:structOnObject');
-    scfg = struct(cfg);
-    if isa(cfg,'dr.dr_cfg')
-        p = properties(dr.dr_cfg([]));
-            scfg = rmfield(scfg,p);
-    end
-    warning('on','MATLAB:structOnObject');
-end
