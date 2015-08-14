226dcb0b3bdbdad0efeef1f2b41a743cf458aa9b
Denis Rozumnyi <rozumden@lcnode1.felk.cvut.cz>
2015-08-14 14:38:13 +0200

    changes

---
+++ +LINE/LsdLines.m
@@ -0,0 +1,12 @@
+classdef LsdLines < Gen
+    methods
+        function this = LsdLines()
+        end
+    end
+    
+    methods(Static)
+        function res = make(img,feat_cfg_list,varargin)
+            disp(['LINE detection ' img.url ':']);
+        end
+    end
+end
