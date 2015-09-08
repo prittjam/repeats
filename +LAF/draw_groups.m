2ac2961c7e35c0f1112ee3922c9eb641e4b2223b
Denis Rozumnyi <rozumden@lcnode1.felk.cvut.cz>
2015-09-08 12:14:20 +0200

    update

--- +LAF/draw_groups.m
+++ +LAF/draw_groups.m
@@ -1,5 +1,6 @@
 function draw_repeats(ax0,u,labeling,varargin)
 cfg.exclude = 0;
+cfg.color = '';
 [cfg,leftover] = helpers.vl_argparse(cfg,varargin{:});
 if isempty(leftover)
     leftover = {'LineWidth',3};
@@ -15,6 +16,9 @@ mu = [(u(1:2,:)+u(4:5,:)+u(7:8,:))/3];
 for k = 1:numel(udraw_labeling)
     idx = find(labeling == udraw_labeling(k));
     color = mpdc(find(cmap==udraw_labeling(k)),:);
+    if ~isempty(cfg.color)
+    	color = cfg.color;
+    end
     LAF.draw(ax0,u(:,idx),'Color',color,leftover{:});
     text(mu(1,idx)',mu(2,idx)', ...
          num2str(udraw_labeling(k)), ...
