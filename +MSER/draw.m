9e26f361bd490eea50475932418522babd3e029b
Denis Rozumnyi <rozumden@lcnode1.felk.cvut.cz>
2015-09-15 14:20:45 +0200

    new mser drawing

--- +MSER/draw.m
+++ +MSER/draw.m
@@ -2,11 +2,9 @@ function draw(ax1,msers,varargin)
 hold on;
 colors = varycolor(size(msers.rle,2));
 for k = 1:size(msers.rle,2)
-    m = msers.rle{1,k};
-    pts = m{1};
-    plot(ax1,pts(1,:)+0.5,pts(2,:)+0.5,varargin{:});
+    pts = msers.rle{3,k} + 0.5;
+    plot(ax1,pts(1,:),pts(2,:),varargin{:});
 end
-
 axis ij;
 axis equal;
 hold off;
