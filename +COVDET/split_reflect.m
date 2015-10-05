d4f03ab15bd9f60cf3173f8e75d95030a82f223e
Denis Rozumnyi <rozumden@lcnode1.felk.cvut.cz>
2015-10-05 12:43:55 +0200

    split_reflect

---
+++ +COVDET/split_reflect.m
@@ -0,0 +1,9 @@
+function affpt = split_reflect(affpt)
+is_reflected = [affpt.reflected];
+if isfield(affpt,'class')
+	drid = num2cell([affpt(is_reflected).class] * 2);
+	[affpt(is_reflected).class] = deal(drid{:});
+elseif isfield(affpt,'drid')
+	drid = num2cell([affpt(is_reflected).drid] * 2);
+	[affpt(is_reflected).drid] = deal(drid{:});
+end
