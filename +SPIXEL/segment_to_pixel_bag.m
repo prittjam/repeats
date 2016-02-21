1efa59c74383c5c2e80f93215680118c12c4b077
James Pritts <prittjam@cmp.felk.cvut.cz>
2016-02-21 19:07:53 +0100

    added

--- +SPIXEL/segment_to_pixel_bag.m
+++ +SPIXEL/segment_to_pixel_bag.m
@@ -1,4 +1,4 @@
-function c = segments_to_pixel_bags(segments,img,varargin)
+function c = segment_to_pixel_bag(segments,img,varargin)
 cfg.sigma = 0;
 cfg.decimation_factor = 1;
 [cfg,leftover] = cmp_argparse(cfg,varargin{:});
@@ -15,9 +15,9 @@ end
 for active = [min_segments:max_segments]
     BW = ismember(segments,active);
     if cfg.decimation_factor == 1
-        c{active} = mask_to_pixels(BW,img);
+        c{active} = mask_to_pixel_bag(BW,img);
     else
-        X = mask_to_pixels(BW,img);
+        X = mask_to_pixel_bag(BW,img);
         num_px = size(X,2);
         idx = randperm(num_px,ceil(1/cfg.decimation_factor*num_px));
         c{active} = X(:,idx);
