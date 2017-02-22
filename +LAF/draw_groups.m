f2d3980d4682ede63a20792618e52b610f0dd168
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-02-22 15:25:41 +0100

    changes

--- +LAF/draw_groups.m
+++ +LAF/draw_groups.m
@@ -10,9 +10,9 @@ if isstruct(u)
     u = [u(:).u];
 end
 
-if isempty(leftover)
-    leftover = { {} };
-end
+%if isempty(leftover)
+%    leftover = { {} };
+%end
 
 Lia = ismember(G,cfg.exclude);
 G(find(Lia)) = nan;
@@ -22,22 +22,26 @@ cmap_dc = distinguishable_colors(numel(uG)+1);
 colors = zeros(numel(G),3);
 colors(G>0,:) = cmap_dc(G(G>0),:);
 
-cmp_splitapply(@(v,color) draw_one_group(ax0,v,color,cfg.linewidth,leftover{:}),...
-               u,colors',G);
+
+if isempty(leftover)
+    cmp_splitapply(@(v,color) ...
+                   draw_one_group(ax0,v,color,cfg.linewidth),...
+                   u,colors',G);
+else
+    cmp_splitapply(@(v,color) ...
+                   draw_one_group(ax0,v,color,cfg.linewidth,leftover{:}),...
+                   u,colors',G);
+end
 
 if cfg.printlabels
     cmp_splitapply(@(u,id,color) print_group_labels(ax0,u,id,color), ...
                    u,colors',G,G);
 end
 
-function [] = draw_one_group(ax0,u,color,linewidth,leftover)
-LAF.draw(ax0,u,'Color','black', ...
-         'LineWidth',linewidth+2, ...
-         leftover{:});
+function [] = draw_one_group(ax0,u,color,linewidth,varargin)
+LAF.draw(ax0,u,'Color','black', 'LineWidth',linewidth+2,varargin{:});
 su = LAF.shrink(u,1.5);
-LAF.draw(ax0,su,'Color',color(:,1), ...
-         'LineWidth',linewidth, ...
-         leftover{:});
+LAF.draw(ax0,su,'Color',color(:,1),'LineWidth',linewidth,varargin{:});
 
 function [] = print_group_labels(ax0,u,color,id)
 mu = [(u(1:2,:)+u(4:5,:)+u(7:8,:))/3];
