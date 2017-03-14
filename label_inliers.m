ac4799587c5d16103df4940102307d31037d068c
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-03-14 20:20:23 +0100

    updates

---
+++ label_inliers.m
@@ -0,0 +1,10 @@
+function G = label_inliers(err)
+sigma = max([1.4826*mad(err) 1]);
+err = reshape_err(err);
+err2 = sum(err.^2);
+G = err2 < 21.026*sigma^2;
+
+function err = reshape_err(err)
+err = reshape(err,6,[]);
+err = [ err(:,1:end/2); ...
+        err(:,(end/2+1):end) ];
