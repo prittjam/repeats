6061c0d7efb9ba3b24f8f846ba213a52cdcede3d
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-03-15 16:43:17 +0100

    changes

--- +HG/laf2xNxN_to_RtxNxN.m
+++ +HG/laf2xNxN_to_RtxNxN.m
@@ -1,5 +1,5 @@
-function [Rt,ii,jj,is_reflected] = laf2xNxN_to_RtxNxN(u,varargin)
-cfg.do_reflection = false;
+function [Rt,ii,jj,is_inverted] = laf2xNxN_to_RtxNxN(u,varargin)
+cfg.do_inversion = false;
 cfg.motion_model = 'laf2xN_to_RtxN';
 [cfg,leftover] = cmp_argparse(cfg,varargin{:});
 
@@ -7,18 +7,14 @@ N = size(u,2);
 [ii,jj] = itril([N N],-1);
 
 Rt = feval(cfg.motion_model,[u(:,ii);u(:,jj)]);
-is_reflected = false(1,N);
+is_inverted = false(1,N);
 
-if cfg.do_reflection
-    is_reflected = Rt(2,:) < 0;
-    Rt(1,is_reflected) = -1*Rt(1,is_reflected);
-    Rt(2:3,is_reflected) = PT.apply_rigid_xforms(-1*Rt(2:3,is_reflected), ...
-                                                 Rt(1,is_reflected), ...
-                                                 zeros(2,sum(is_reflected)));
-    [jj(is_reflected),ii(is_reflected)] = deal(ii(is_reflected), ...
-                                               jj(is_reflected));
+if cfg.do_inversion
+    is_inverted = Rt(2,:) < 0;
+    Rt(1,is_inverted) = -1*Rt(1,is_inverted);
+    Rt(2:3,is_inverted) = PT.apply_rigid_xforms(-1*Rt(2:3,is_inverted), ...
+                                                Rt(1,is_inverted), ...
+                                                zeros(2,sum(is_inverted)));
+    [jj(is_inverted),ii(is_inverted)] = deal(ii(is_inverted), ...
+                                             jj(is_inverted));
 end
-
-%K = numel(Rt);
-%Rt = struct('theta',mat2cell(theta,1,ones(1,K)), ...
-%            't', mat2cell(t,2,ones(1,K)));
