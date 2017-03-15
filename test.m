8c7a65304c9706aa0ce745d8cb6b709149887923
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-03-15 19:00:19 +0100

    changes

--- test.m
+++ test.m
@@ -21,8 +21,8 @@ greedy_repeats_init();
 %listA  = {'wall.jpg'};
 %listA = {'EsherA.jpg'};
 %listA = { 'DSC_0810.jpg' }
-listA = { 'fireengine1.jpg' };
-%listA = {'liz_taylor.jpg'};
+%listA = { 'fireengine1.jpg' };
+listA = {'liz_taylor.jpg'};
 %listA = {'object0149.view01.png'};
 %listA = {'lviv5.jpg'};
 %listA = {'minnesota-tennis-courts-aerial.jpg'}
@@ -58,14 +58,12 @@ dr = dr(randperm(numel(dr)));
 tmp = mat2cell(drid2,1,ones(1,numel(drid2)));
 [dr(:).drid] = tmp{:};
 
-u = [dr(:).u];
-
 gr_params = { 'desc_linkage', 'single', ...
               'desc_cutoff', 150,... 
               'cc', [img.width/2 img.height/2], ...
               'motion_model', 'HG.laf2xN_to_txN', ...
               'img', img.data, ...
-              'num_planes', 2 };
+              'num_planes', 1 };
 
 [model_list,u_corr_list,stats_list] = greedy_repeats(dr,gr_params{:});
 
@@ -74,34 +72,3 @@ for k = 1:numel(model_list)
     h = draw_results(img,rimg);
     draw_reconstruction(h(1),u_corr_list{k},model_list{k});
 end
-% 
-%ires = res;
-%ores = res;
-%ires.u_corr = ires.u_corr(logical(res.G),:);
-%ores.u_corr = ores.u_corr(~logical(res.G),:);
-%
-%%draw_motions(ires,img.data,'dr',dr,'Color','c');
-%
-%ores0 = res0;
-%%ores0.u_corr = ores0.u_corr(~logical(res0.G),:);
-%if ~isempty(ores0.u_corr)
-%    draw_motions(ores0,img.data,'dr',dr);
-%end
-%drawnow('update') ;
-%%output_results(img,res);
-%
-%if ~isempty(ores.u_corr)
-%    draw_motions(ores,img.data,'dr',dr);
-%end
-%drawnow('update') ;
-%
-%
-%figure;
-%subplot(2,2,1);
-%plot(stats.rho_0);
-%subplot(2,2,2);
-%plot(stats.l2_0);
-%subplot(2,2,3);
-%plot(stats.rho);
-%subplot(2,2,4);
-%plot(stats.l2);
