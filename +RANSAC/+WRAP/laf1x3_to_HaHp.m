d35c302fda27bb0a982989763c73222939692eb6
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-04-24 16:23:39 +0200

    changes

--- +RANSAC/+WRAP/laf1x3_to_HaHp.m
+++ +RANSAC/+WRAP/laf1x3_to_HaHp.m
@@ -1,13 +1,5 @@
 % Copyright (c) 2017 James Pritts
 % 
-% Permission is hereby granted, free of charge, to any person obtaining a copy
-% of this software and associated documentation files (the "Software"), to deal
-% in the Software without restriction, subject to the following conditions:
-% 
-% The above copyright notice and this permission notice shall be included in 
-% all copies or substantial portions of the Software.
-%
-% The Software is provided "as is", without warranty of any kind.
 classdef laf1x3_to_HaHp
     properties
         mss = 3;
