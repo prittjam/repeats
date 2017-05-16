fb970d267eb64090c3f0358d093a581841d0a167
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-05-16 19:01:15 +0200

    changes

--- output_results.m
+++
@@ -1,8 +0,0 @@
-function [] = output_results(img,rimg,ud_img)
-[~,file_name,file_ext] = fileparts(img.url);
-if ~isempty(rimg)
-    imwrite(rimg,['rect_' file_name file_ext]); 
-end
-if nargin == 3
-    imwrite(ud_img,['ud_' file_name file_ext]);    
-end
