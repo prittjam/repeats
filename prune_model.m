a46ca9f4ce85e69d24528f14ec3e162b1bedea82
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-05-22 21:47:48 +0200

    changes

--- prune_model.m
+++ prune_model.m
@@ -1,3 +1,6 @@
 function [u_corr,model,stats] = prune_model(u,u_corr,model)
-kkk = 3;
-keb
+keyboard;
+s = [1 1 1 2 5 3 6 4 7 8 8 8];
+t = [2 3 4 5 3 6 4 7 2 6 7 5];
+weights = [100 10 10 10 10 20 10 30 50 10 70 10];
+G = graph(s,t,weights);
