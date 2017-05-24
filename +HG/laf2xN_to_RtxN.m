3188a1b8e3228bb0a41c0b59583df852ccc3a33d
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-05-24 17:33:23 +0200

    changes

--- +HG/laf2xN_to_RtxN.m
+++ +HG/laf2xN_to_RtxN.m
@@ -6,6 +6,7 @@ u_ii = u(1:9,:);
 u_jj = u(10:18,:);
 s1 = LAF.is_right_handed(u_ii);
 s2 = LAF.is_right_handed(u_jj);
+
 hands_switched = xor(s1,s2);
 u_ii(:,hands_switched) = LAF.reflect_yaxis(u_ii(:,hands_switched));
 
--- +Rt/invert.m
+++ +Rt/invert.m
@@ -1,7 +1,11 @@
 function Rt = invert(Rt)
-    Rt(1,:) = -Rt(1,:);
     c = cos(Rt(1,:));
     s = sin(Rt(1,:));
+
     a11 = Rt(4,:);
-    Rt(2:3,:) = -[ a11.*c.*Rt(2,:)-a11.*s.*Rt(3,:); ...
-                   s.*Rt(2,:)+c.*Rt(3,:) ]; 
+    is_reflected =  a11 == -1;
+    
+    Rt(1,~is_reflected) = -Rt(1,~is_reflected);
+    
+    Rt(2:3,:) = [ -a11.*(c.*Rt(2,:)+s.*Rt(3,:)); ...
+                  s.*Rt(2,:)-c.*Rt(3,:) ]; 
