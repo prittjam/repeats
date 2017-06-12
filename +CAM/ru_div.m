c8dd40fba60797366e77245c9ca069e32d3284c3
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-06-12 16:04:14 +0200

    changes

---
+++ +CAM/ru_div.m
@@ -0,0 +1,33 @@
+function w = ru_div(u,cc,la)
+%UDERAD - removes additive radial distortion from coordinates
+%     function u = uderad(u, cc, la)
+%     where cc is image centre and la is the parameter of the
+%     radial distortion (see AWF: Simult. lin. est...)
+%
+%SEE ALSO: URAD, INORMU
+
+if abs(la) > eps
+    dim = size(u,1);
+    if (dim == 2)
+        u = [u;ones(1,size(u,2))];
+    end
+
+    sc = sum(2*cc);
+    A = [1/sc   0  -cc(1)/sc; ...
+         0   1/sc  -cc(2)/sc; ...
+         0     0       1];
+
+    v = A*u;
+
+    dv = 1+la*(v(1,:).^2+v(2,:).^2);
+    v(1,:)  = v(1,:)./dv; 
+    v(2,:)  = v(2,:)./dv; 
+
+    w = inv(A)*v;
+ 
+    if (dim == 2)
+        w = w(1:2,:);
+    end
+else
+    w = u; 
+end
