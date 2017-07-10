5638945b96ad066ff83c9bdcbb15d1e93ef357b8
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-07-10 19:27:00 +0200

    changes

--- +SIM/+ARC/sample.m
+++ +SIM/+ARC/sample.m
@@ -14,3 +14,11 @@ for k = 1:numel(arc_list)
     X{k} = [x(:,in(ind)); ...
             ones(1,numel(ind))];
 end
+
+function [x,y] = sample_circle(x,circ)
+a = 1;
+b = -2*circ.c(2);
+c = x.^2-2*x.*circ.c(1)+circ.c(1)^2+circ.c(2)^2-circ.r^2;
+
+y1 = -b+sqrt(b^2-4.*a.*c)/2./a;
+y2 = -b+sqrt(b^2-4.*a.*c)/2./a;
