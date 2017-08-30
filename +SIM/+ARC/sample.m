2f0e401614be12b55a561542d171142637052d42
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-08-30 16:19:06 +0200

    changes

--- +SIM/+ARC/sample.m
+++ +SIM/+ARC/sample.m
@@ -9,7 +9,6 @@ for k = 1:numel(arc_list)
               0.5 ny; ...
               0.5 0.5];
     in = find(inpolygon(x(1,:),x(2,:),border(:,1)',border(:,2)'));
-    keyboard;
     ind = randperm(numel(in),cfg.num_points);
     X{k} = [x(:,in(ind)); ...
             ones(1,numel(ind))];
@@ -18,11 +17,8 @@ end
 function z = sample_circle(x,circ)
 inl = abs(x-circ.c(1)) <= circ.r;
 x = x(inl);
-
 a = 1;
 b = -2*circ.c(2);
 c = x.^2-2*x.*circ.c(1)+circ.c(1)^2+circ.c(2)^2-circ.r^2;
-
 y = [-b+sqrt(b^2-4.*a.*c) -b-sqrt(b^2-4.*a.*c)]./2./a;
-
 z = [x x; y];
