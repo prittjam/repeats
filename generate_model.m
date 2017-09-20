ca72896c3b890407c308998ebd7d6b77c81afd7e
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-09-20 18:02:36 +0200

    changes

--- generate_model.m
+++
@@ -1,9 +0,0 @@
-function [model,corresp,ransac_res,ransac_stats] = ...
-        generate_model(dr,motion_model,cc,varargin)
-    u = [dr(:).u];
-    Gapp = [dr(:).Gapp];
-    corresp = cmp_splitapply(@(u) { VChooseK(u,2)' }, ...
-                             1:numel(Gapp),Gapp);
-    corresp = [ corresp{:} ];
-    ransac = make_ransac(dr,corresp,cc,motion_model);
-    [model,ransac_res,ransac_stats] = ransac.fit(dr,corresp);
