d31cc1187c84c3cc272b87c037eed8c3417d6302
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-09-20 18:02:26 +0200

    changes

--- +RANSAC/+WRAP/laf2x2_to_HaHp.m
+++ +RANSAC/+WRAP/laf2x2_to_HaHp.m
@@ -1,13 +1,5 @@
 % Copyright (c) 2017 James Pritts
 % 
-% Permission is hereby granted, free of charge, to any person obtaining a copy
-% of this software and associated documentation files (the "Software"), to deal
-% in the Software without restriction, subject to the following conditions:
-% 
-% The above copyright notice and this permission notice shall be included in 
-% all copies or substantial portions of the Software.
-%
-% The Software is provided "as is", without warranty of any kind.
 classdef laf2x2_to_HaHp
     properties
         mcs = 2;
@@ -27,11 +19,11 @@ classdef laf2x2_to_HaHp
             Hp = HG.laf2x2_to_Hinf(x,G);
             xp = LAF.renormI(blkdiag(Hp,Hp,Hp)*x);
             Ha = HG.laf2x1_to_Amu(xp,G);
-
+            
             if ~isempty(Ha)
-                M = { Ha*Hp } ;
+                M.H = Ha*Hp ;
             else
-                M  = { Hp };
+                M.H = Hp;
             end
         end
 
@@ -44,7 +36,7 @@ classdef laf2x2_to_HaHp
         function is_good = is_model_good(this,dr,corresp,idx,M) 
             m  = reshape(corresp(:,idx),1,[]);
             x = [dr(m).u]; 
-            is_good = LAF.are_same_orientation(x,M(3,:)'); 
+            is_good = LAF.are_same_orientation(x,M.H(3,:)'); 
         end        
         
         function M = fix(this,dr,corresp,idx,model)
