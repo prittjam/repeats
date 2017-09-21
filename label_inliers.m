90b8a5fe9966edc70259a755286048f805ea7478
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-09-21 17:31:08 +0200

    changes

--- label_inliers.m
+++
@@ -1,7 +0,0 @@
-function [G,err2] = label_inliers(err)
-sigma = max([1.4826*mad(err) 1]);
-err2 = reshape(err.^2,6,[]);
-err2 = [ err2(:,1:end/2); ...
-         err2(:,(end/2+1):end) ];
-err2 = sum(err2);
-G = err2 < 21.026*sigma^2;
