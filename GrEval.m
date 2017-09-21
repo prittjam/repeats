7f32dce1ab5154a92dfa1f26bc9202b412c2079c
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-09-21 17:33:52 +0200

    changes

--- GrEval.m
+++ GrEval.m
@@ -1,92 +1,31 @@
-classdef GrEval < handle
+classdef GrEval2 < handle
     properties    
-        sigma = 1;
-        iff = [];
-        motion_model = [];
-        motion_solver = @HG.laf2xN_to_RtxN;
-        T = [];
-    end
-    
-    methods(Static)        
-        function E = calc_error_impl(u,v,invH,motion_solver,cutoff)
-            N = size(u,2);
-
-            [ii,jj] = itril([N N],-1);
-            rt = feval(motion_solver,[v(:,ii);v(:,jj)]);            
-            
-            [rt,is_inverted] = unique_ro(rt);
-                
-            [jj(is_inverted),ii(is_inverted)] = yxdeal(ii(is_inverted), ...
-                                                     jj(is_inverted));
-            
-            invrt = Rt.invert(rt);
-            ut1 = LAF.renormI(blkdiag(invH,invH,invH)* ...
-                              LAF.apply_rigid_xforms(v(:,ii),rt));
-            ut2 = LAF.renormI(blkdiag(invH,invH,invH)* ...
-                              LAF.apply_rigid_xforms(v(:,jj),invrt));
-
-            Y = sum([ut1-u(:,jj); ...
-                     ut2-u(:,ii)].^2);
-
-            Z = linkage(Y,'complete');
-            G = cluster(Z,'cutoff',cutoff,'criterion','distance');
-
-            freq = hist(G,1:max(G));            
-            
-            [max_freq,maxG] = max(freq);
-
-            E = ones(1,size(u,2));
-            if max_freq > 1
-                E(find(G == maxG)) = 0;
-            end
-        end 
+        T = log(1.1);
     end
     
     methods
-        function this = GrEval(varargin)
-            [this,~] = cmp_argparse(this,varargin{:});
-            this.iff = @(x) make_iif(sum(x < this.T) > 1, ...
-                                     @() x < this.T, ...
-                                     true, ...
-                                     @() false(1,numel(x)));
-            this.T = 20*this.sigma^2;
-            
-            switch this.motion_model
-              case 't'
-                this.motion_solver = @HG.laf2xN_to_txN;
-              case 'Rt'
-                this.motion_solver = @HG.laf2xN_to_RtxN;
-            end
+        function this = GrEval2(varargin)
+            this = cmp_argparse(this,varargin{:});
         end        
         
-        function [loss,E] = calc_loss(this,dr,G,H)         
-            invH = inv(H);
+        function [loss,E] = calc_loss(this,dr,corresp,M)         
+            H = M.H;
+            v = LAF.renormI(blkdiag(H,H,H)*[dr(:).u]);
             
-            u = [dr(:).u];
-            v = LAF.renormI(blkdiag(H,H,H)*u);
-            
-            E = zeros(1,numel(G));
-            
-            uG = unique(G(~isnan(G)));
-            
-            for g = uG
-                ind = find(G == g);
-                E(ind) = GrEval.calc_error_impl(u(:,ind),v(:,ind), ...
-                                                invH,this.motion_solver,this.T);
-            end
-            
-            %            E = ...
-            %                msplitapply(@(uu,vv) ...
-            %                            GrEval.calc_error_impl(uu,vv, ...
-            %                                                   invH,this.motion_model,this.T) , ...
-            %                            u,v,findgroups(G)); 
+            D2 = [sum((v(1:2,:)-v(4:5,:)).^2); ...
+                  sum((v(7:8,:)-v(4:5,:)).^2); ...
+                  sum((v(7:8,:)-v(1:2,:)).^2)];
+            lr = 0.5*(log(D2(:,corresp(2,:)))-log(D2(:,corresp(1,:))));
 
-            E(isnan(E)) = 1;
+            E = max(abs(lr)); 
+
+            E(isnan(E)) = this.T;
+            E(E > this.T) = this.T;
             loss = sum(E);
         end        
         
         function cs = calc_cs(this,E)
-            cs = 1-E;
+            cs = E < this.T;
         end                        
     end
 end
