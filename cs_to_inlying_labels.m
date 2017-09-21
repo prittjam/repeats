bd08313acd73ed2001c79c31d4b52c3dffa5ffab
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-09-21 17:30:25 +0200

    changes

--- cs_to_inlying_labels.m
+++
@@ -1,4 +0,0 @@
-function G = cs_to_inlying_labels(labeling,cs,IJ)
-G = labeling.*cs;
-G(G==0) = nan;
-G = findgroups(G);
--- verify_geometry.m
+++
@@ -1,2 +0,0 @@
-function G_sv = verify_geometry(G_app,cs)
-G_sv = cs_to_inlying_labels(G_app,cs);
