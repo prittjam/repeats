2794ee7b8d7b73d0bc715729af0645c1bf408506
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-10-05 18:47:10 +0200

    changes

--- composite_xforms.m
+++ composite_xforms.m
@@ -1,8 +1,9 @@
-function [Gs,Rti,dRti_dj] = composite_xforms(Tlist,Gm,inverted,...
-                                             Rtij,X,num_vertices)
+function [Gs,Rti,Gmj] = composite_xforms(Tlist,Gm,inverted,...
+                                         Rtij,X,num_vertices)
 mtx_Rtij = Rt.params_to_mtx(Rtij);
 inv_mtx_Rtij = Rt.params_to_mtx(Rt.invert(Rtij));
 Gs =  nan(num_vertices,1);
+Gmj = nan(num_vertices,1);
 mtxRti = repmat(eye(3,3),1,1,num_vertices);
 
 for k1 = 1:numel(Tlist)
@@ -10,14 +11,15 @@ for k1 = 1:numel(Tlist)
     Gs(T(1,1)) = k1;
     for k2 = 1:size(T,1);
         gm = Gm(T(k2,1),T(k2,2));
+        Gmj([T(k2,1) T(k2,2)]) = gm;
         if inverted(T(k2,1),T(k2,2))
-            mtxRti(:,:,T(k2,2)) = inv_mtx_Rtij(:,:,gm)*mtxRti(:,:, ...
-                                                              T(k2,1));
+            mtxRti(:,:,T(k2,2)) = ...
+                inv_mtx_Rtij(:,:,gm)*mtxRti(:,:,T(k2,1));
         else
             mtxRti(:,:,T(k2,2)) = mtx_Rtij(:,:,gm)*mtxRti(:,:,T(k2,1));
         end
         Gs(T(k2,2)) = k1;
     end
 end
-Rti = Rt.mtx_to_params(mtxRti)';
 
+Rti = Rt.mtx_to_params(mtxRti)';
