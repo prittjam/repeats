d46aa336846297747ab1ee60c503b6dd4d2770e3
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-10-20 16:43:59 +0200

    changes

--- +IMG/output_rectification.m
+++ +IMG/output_rectification.m
@@ -1,20 +1,11 @@
-function [] = output_rectification(img,rimg,ud_img,prefix)
+function [] = output_rectification(url,rimg,prefix)
 if nargin < 3
-    ud_img = [];
-end
-
-if nargin < 4
     prefix = pwd;
 end
 
-[~,file_name,file_ext] = fileparts(img.url);
+[~,file_name,file_ext] = fileparts(url);
 if ~isempty(rimg)
     rfile_name = ['rect_' file_name file_ext];
     url = fullfile(prefix,rfile_name);
     imwrite(rimg,url); 
-end
-if ~isempty(ud_img)
-    udfile_name = ['ud_' file_name file_ext];
-    url = fullfile(prefix,udfile_name);
-    imwrite(ud_img,url);    
 end
