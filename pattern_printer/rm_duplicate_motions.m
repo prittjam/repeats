e53c090f921d581a1c6ccb08550ede50bee833c3
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-10-31 14:11:25 +0100

    removed pattern printer

--- pattern_printer/rm_duplicate_motions.m
+++
@@ -1,9 +0,0 @@
-function w = rm_duplicate_codes(K,w)
-for k = 1:numel(w)
-    w1 = w;
-    w1(k) = 0;
-    b = K*w1; 
-    if all(b > 1e-5)
-        w = w1;
-    end
-end
--- pattern_printer/sfm.m
+++
@@ -1,6 +0,0 @@
-function [Xp,inl] = sfm(X,Gs,Rti)
-inl = find(~isnan(Gs));
-inlGs = reshape(Gs(inl),1,[]);
-Y = X(:,inlGs);
-Rti = [Rti(inl,:)]';
-Xp = LAF.apply_rigid_xforms(Y,Rti);
