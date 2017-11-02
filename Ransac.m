816716a572eadc5f96692a69ad42dcaf99a5ce0a
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-11-02 16:30:46 +0100

    changes

--- Ransac.m
+++ Ransac.m
@@ -27,16 +27,16 @@ classdef Ransac < handle
             this.eval = eval;
         end
         
-        function [loM,lo_res] = do_lo(this,meas,corresp,M,res)
+        function [loM,lo_res] = do_lo(this,x,corresp,M,res,varargin)
             loM = [];
             lo_res = [];
             if ~isempty(this.lo)
-                [loM,lo_res] = this.lo.fit(meas,corresp,M,res);
+                [loM,lo_res] = this.lo.fit(x,corresp,M,res,varargin{:});
                 this.stats.lo_count = this.stats.lo_count+1;
             end
         end
 
-        function  [optM,opt_res,M,res,stats] = fit(this,meas,corresp)
+        function  [optM,opt_res,M,res,stats] = fit(this,x,corresp,varargin)
             tic;
 
             this.stats = struct('time_elapsed', 0, ...
@@ -55,13 +55,14 @@ classdef Ransac < handle
             has_model = false;
             while true         
                 for k = 1:this.sampler.max_num_retries
-                    idx = this.sampler.sample(meas,corresp);
+                    idx = this.sampler.sample(x,corresp);
                     is_sample_good = ...
-                        this.model.is_sample_good(meas,corresp,idx);
+                        this.model.is_sample_good(x,corresp,idx);
                     if is_sample_good
                         try
-                            model_list = this.model.fit(meas, ...
-                                                        corresp,idx);
+                            model_list = this.model.fit(x, ...
+                                                        corresp,idx, ...
+                                                        varargin{:});
                         catch
                             model_list = [];
                         end
@@ -84,13 +85,13 @@ classdef Ransac < handle
                 is_model_good = false(1,numel(model_list));
                 for k = 1:numel(model_list)
                     is_model_good(k) = ...
-                        this.model.is_model_good(meas,corresp,idx,model_list(k));
+                        this.model.is_model_good(x,corresp,idx,model_list(k));
                 end
                 
                 if ~all(is_model_good)
                     bad_ind = find(~is_model_good);
                     for k = bad_ind
-                        Mfix = this.model.fix(meas,corresp,idx,model_list(k));
+                        Mfix = this.model.fix(x,corresp,idx,model_list(k));
                         if ~isempty(Mfix)
                             is_model_good(k) = true;
                             model_list(k) = Mfix;
@@ -105,7 +106,7 @@ classdef Ransac < handle
                     loss = inf(numel(model_list),1);
 
                     for k = 1:numel(model_list)
-                        [loss(k),err{k}] = this.eval.calc_loss(meas,corresp,model_list(k));
+                        [loss(k),err{k}] = this.eval.calc_loss(x,corresp,model_list(k));
                         cs{k} = this.eval.calc_cs(err{k});
                     end
                     
@@ -121,9 +122,8 @@ classdef Ransac < handle
                             (sum(res0.cs) >= sum(res.cs))
                         M = M0;
                         res = res0;
-                        [loM,lo_res] = this.do_lo(meas,corresp,M0,res0); 
+                        [loM,lo_res] = this.do_lo(x,corresp,M0,res0,varargin{:}); 
                         if (lo_res.loss < opt_res.loss)
-
                             optM = loM;
                             opt_res = lo_res;
                         end
@@ -142,7 +142,7 @@ classdef Ransac < handle
             end
             
             if this.stats.lo_count == 0
-                [optM,opt_res] = this.do_lo(meas,corresp,M,res);
+                [optM,opt_res] = this.do_lo(x,corresp,M,res);
             end
             
             this.stats.time_elapsed = toc;               
