8ca269c774f1be69e5ff20a40e6b08c0a3728d77
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-11-05 15:40:37 +0100

    changes

--- +DR/group_desc.m
+++ +DR/group_desc.m
@@ -1,48 +1,13 @@
 function [Gapp,Gr] = group_desc(dr,varargin)
-cfg.desc_cutoff = 150;
-cfg.desc_linkage = 'single';
-cfg.group_reflections = true;
-cfg.rm_duplicates = false;
-
 [cfg,leftover] = cmp_argparse(cfg,varargin{:});
+G = DR.group_desc_app(dr);
+Gr = DR.group_reflections(dr);
 
-N = numel(dr);
+%if cfg.rm_duplicates
+%    Gapp = findgroups(msplitapply(@(dr,G) rm_duplicates([dr(:).u],G),dr,Gapp,Gapp));
+%end
 
-unames = categories([dr(:).uname]);
-is_reflected = cellfun(@(u) numel(strfind(u,'ReflectImg:')),unames);
-dr_str = cellfun(@(u) strrep(u,'ReflectImg:',''),unames, ...
-                 'UniformOutput',false);
-Guname = findgroups([dr.uname]);
-uGsdr = conncomp(graph(squareform(pdist(dr_str,@strcmp)) > 0));
-Gsdr = uGsdr(Guname);
-
-[T,idx] =  ...
-    splitapply(@(x,y) ...
-               deal({clusterdata(single([x(:).desc]'), ...
-                                 'linkage',cfg.desc_linkage, ...
-                                 'criterion','distance', ...
-                                 'cutoff',cfg.desc_cutoff)},{y}), ... 
-               dr,1:numel(dr),findgroups(Gsdr));
-
-G = zeros(size(Gsdr));
-maxT = max(G);
-for k = 1:numel(T)
-    G(idx{k}) = T{k}+maxT;
-    maxT = max(G);
-end
-
-freq = hist(G,1:max(G));
-[~,idxb] = ismember(find(freq == 1),G);
-G(idxb) = nan;
-
-Gapp = reshape(findgroups(G),1,[]);
-
-if cfg.rm_duplicates
-    Gapp = findgroups(msplitapply(@(dr,G) rm_duplicates([dr(:).u],G),dr,Gapp,Gapp));
-end
-    
-Gr = reshape(is_reflected(findgroups([dr.uname])),1,[]);
-
+keyboard;
 if ~cfg.group_reflections
     Gr_tmp = Gr;
     Gr_tmp(find(Gr_tmp == 0)) = -1;
@@ -54,17 +19,7 @@ if ~cfg.group_reflections
     Gapp = findgroups(Gapp);
 end
 
-function G = rm_duplicates(u,G)
-N = size(u,2);
-d2 = pdist(u','euclidean');
-ind = find(d2 < 20);
-if ~isempty(ind)
-    [I,J] = itril(N,-1);
-    I = I(ind);
-    J = J(ind);
-    G([I;J]) = nan;
-    num_good = sum(~isnan(G));
-    if num_good == 1
-        G = nan(size(G));
-    end
-end
+
+
+
+
