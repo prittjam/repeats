286f8415d4dc7b7d0cf3e5bd07f7a4d16a4e681f
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-11-08 18:21:35 +0100

    changes

--- +LAF/make_random.m
+++ +LAF/make_random.m
@@ -4,7 +4,7 @@ theta = pi/3*rand(1,num_lafs)+pi/3;
 s = sin(theta);
 c = cos(theta);
 sc = 3*rand(1,num_lafs)+0.1;
-x3 = sc.*[c.*x1(1,:)-s.*x1(2,:);s.*x1(1,:)+c.*x1(2,:)];
+x3 = bsxfun(@times,sc,[c.*x1(1,:)-s.*x1(2,:);s.*x1(1,:)+c.*x1(2,:)]);
 xc = [x1;ones(1,num_lafs);zeros(2,num_lafs);ones(1,num_lafs);x3; ...
       ones(1,num_lafs)];
 xc = LAF.apply_rigid_xforms(xc,[2*pi*rand(1,num_lafs);zeros(2,num_lafs);ones(1,num_lafs)]);
