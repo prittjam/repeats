acfe0bd9a4f027204694cec7fe33ea8819159dd5
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-11-09 20:24:39 +0100

    changes

--- fit_coplanar_patterns.m
+++ fit_coplanar_patterns.m
@@ -11,22 +11,26 @@ for k = 1:num_planes
 %    res = struct('cs',cs);
     cs = res0(k).cs;
     
-    keyboard;
     ransac.lo.max_iter = 10;
     [model_list(k),lo_res_list(k)] = ...
         ransac.lo.fit(x,corresp,model0,res0,Gsamp,Gapp);
     
-    Hinf = model_list(k).Hinf;
-    u = LAF.renormI(blkdiag(Hinf,Hinf,Hinf)*...
-                    LAF.ru_div(x,model_list(1).cc, ...
-                               model_list(1).q));
-    Grect = nan(size(Gapp));
-    
-    inl = unique(lo_res_list(k).cs);
+    Grect = nan(size(Gapp));  
+    inl = unique(corresp(:,lo_res_list(k).cs));
     Grect(inl) = findgroups(Gapp(inl));
-    Ha = HG.laf1x2_to_Amu(u,Grect);
-    keyboard;
-    if ~isempty(Ha)
-        model_list(k).Hinf = Ha*model_list(k).Hinf;
+    if (any(LAF.is_right_handed(x(:,inl))))
+        Hinf = model_list(k).Hinf;
+        u = LAF.renormI(blkdiag(Hinf,Hinf,Hinf)*...
+                        LAF.ru_div(x,model_list(1).cc, ...
+                                   model_list(1).q));
+        Ha = HG.laf1x2_to_Amu(u,Grect);
+        if ~isempty(Ha)
+            model_list(k).Hinf = Ha*model_list(k).Hinf;
+        else
+            disp('Metric upgrade failed');
+        end
+    else
+        disp('No reflections detected for metric upgrade');
     end
+
 end
