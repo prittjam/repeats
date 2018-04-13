f2e252786c625fb9eb82af3d484f3f1c59cc7dd8
James Pritts <prittjam@cmp.felk.cvut.cz>
2018-04-13 18:56:44 +0200

    changes

--- repeats_init.m
+++ repeats_init.m
@@ -1,14 +1,11 @@
-function [] = repeats_init(src_path,opt_path)
+function [] = repeats_init()
 [cur_path, name, ext] = fileparts(mfilename('fullpath'));
 
-addpath(genpath(cur_path))
-
-if nargin < 1
-    src_path = '~/src/';
-end
+addpath(genpath(cur_path));
 
 %if ~exist('ColumnType','file')
 %    addpath([src_path '/ckvs']);
 %end
 
 features_init();
+cvdb_init();
