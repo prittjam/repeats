98ed089e6287e2e2539bf5411c2299963ef2f7f1
jbpritts <jbpritts@gmail.com>
2018-06-19 04:28:17 -0400

    changes

--- cvdb/+KEY/class_to_struct.m
+++
@@ -1,4 +0,0 @@
-function scfg = class_to_struct(cfg)
-    warning('off','MATLAB:structOnObject');
-    scfg = struct(cfg);
-    warning('on','MATLAB:structOnObject');
--- cvdb/+SQL/make.m
+++
@@ -1,5 +0,0 @@
-function [] = make_sqldb()
-sql = SQL.SqlDb;
-sql.open();
-sql.clear();
-sql.create();
