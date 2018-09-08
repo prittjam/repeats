455f5d579c82b14da4537fa7484a6f8776ccc31e
jbpritts <jbpritts@gmail.com>
2018-09-08 16:23:38 +0200

    changes

--- vgtk/util/util_make_homogeneous.m
+++
@@ -1,8 +0,0 @@
-%
-%  Copyright (c) 2018 James Pritts
-%  Licensed under the MIT License (see LICENSE for details)
-%
-%  Written by James Pritts
-%
-function v = util_make_homogeneous(u)
-v = [u;ones(1,size(u,2))];
