ecd55812c47cd04b277ba5dd385227a91b50e005
jbpritts <jbpritts@gmail.com>
2018-10-23 14:01:30 +0200

    changes

--- +LAF/warp_fwd.m
+++ +LAF/warp_fwd.m
@@ -6,5 +6,5 @@
 %
 function v = warp_fwd(u,T)
 [x,y] = LAF.pt3x3_to_xy(u);
-[tx,ty] = transformPointsForward(T,x,y);
-v = LAF.xy_to_p3x3(tx,ty);
+[tx,ty] = tformfwd(T,x,y);
+v = LAF.xy_to_pt3x3(tx,ty);
