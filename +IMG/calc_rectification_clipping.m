f9355449df7c692c8822f79905d1100ebd2ededa
jbpritts <jbpritts@gmail.com>
2018-10-24 15:05:53 +0200

    changes

---
+++ +IMG/calc_rectification_clipping.m
@@ -0,0 +1,16 @@
+function border = calc_dscale_border(dims,l,cc,q,minscale,maxscale,pt)
+    [sc_img,si_fn] = IMG.calc_dscale(dims,l,cc,q);
+    if q == 0
+        ref_sc = si_fn(l(1),l(2),1,pt(1),pt(2));
+    else
+        A = [1 0 -cc(1); ...
+             0 1 -cc(2); ...
+             0 0      1];
+        ptn = A*[pt(1) pt(2) 1]';        
+        ref_sc = si_fn(l(1),l(2),q,1,ptn(1),ptn(2));
+    end
+    sc_img = sc_img/ref_sc;
+    mask = (sc_img > minscale) & (sc_img < maxscale);
+    [ii,jj] = find(mask);
+    idx = convhull(ii,jj);
+    border = [jj(idx) ii(idx)];
