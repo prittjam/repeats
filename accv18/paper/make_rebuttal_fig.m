b07894863a0f49063989910dff5096c9edba349d
jbpritts <jbpritts@gmail.com>
2018-10-24 19:18:38 +0200

    changes

--- accv18/paper/make_rebuttal_fig.m
+++ accv18/paper/make_rebuttal_fig.m
@@ -1,3 +1,4 @@
+function [] = make_rebuttal_fig
 close all;
 bright = 1.7;
 img_name = 'cropped_dartboard';
@@ -6,7 +7,6 @@ img = Img('url',['data/' img_name '.jpg']);
 dims = [ny nx];
 dbfile = [ fileparts(which('repeats_init.m')) '/features.db'];
 kvdb = KeyValDb.getObj('dbfile', dbfile);
-
 cid_cache = CidCache(img.cid, ...
                      { 'read_cache', true, ...
                     'write_cache', true });
@@ -14,129 +14,39 @@ dr = DR.get(img,cid_cache, ...
                 {'type','all', ...
                  'reflection', false });
 x = [dr(:).u];
-rect = [80 200 475 875];
-
-cropped = img.data(200:875,80:475,:);
-colors = {'g','r','c'};
 
 load('output/cropped_dartboard_H4ql.mat');
+output_image(x,img,model_list,stats_list);
+
+load('output/cropped_dartboard_H222ql.mat');
+output_image(x,img,model_list,stats_list);
+
+load('output/cropped_dartboard_H32ql.mat');
+output_image(x,img,model_list,stats_list);
+
+
+function [] = output_image(x,img,model_list,stats_list)
+cropped = img.data(200:875,1:555,:);
+rect = [1 200 555 875]-0.5;
+colors = {'g','r','c'};
+
+
 figure;
-image([80 475], [200 875], cropped/bright);
+image([rect(1) rect(2)], [rect(3) rect(4)], cropped);
+axis equal;
+axis tight;
+axis off;
 hold on;
 LAF.draw(gca, x(:,stats_list.ransac(end).res.mss{1}), ...
          'Color','w', 'LineWidth',5);
 LAF.draw(gca, x(:,stats_list.ransac(end).res.mss{1}), ...
          'Color','g','LineWidth',3);
-axis equal;
-axis tight;
-axis off;
 hold off;
-export_fig( ['/home/jbpritts/Documents/accv18_rebuttal/fig/' ...
-             'H4ql.png']);
-ind = find(~isnan(model_list(1).Gs));
-v = reshape(x(:,ind),3,[]);
-
-H = model_list(1).A;
-H(3,:) = transpose(model_list(1).l);
-cc = model_list(1).cc;
-q = model_list(1).q;
-
-boundary = IMG.rect_to_boundary(rect);
-[rimg,trect,tform] = IMG.ru_div_rectify(img.data,H,cc,q, ...
-                                        'cspond', v, ...
-                                        'boundary',boundary, ...
-                                        'Registration','Similarity', ...
-                                        'Dims',dims);
-image([trect(1) trect(2)],[trect(3) trect(4)],rimg);
-axis off;
-axis equal;
-axis ij;
-y = LAF.warp_fwd(x(:,stats_list.ransac(end).res.mss{1}), ...
-                 tform);
-hold on;
-LAF.draw(gca, y, 'Color','w', 'LineWidth',5);
-LAF.draw(gca, y, 'Color','g', 'LineWidth',3);
-hold off;
-
-imwrite(rimg,['/home/jbpritts/Documents/accv18_rebuttal/img/' ...
-         'rect_H4ql.jpg']);
-[uimg,~,trect] = IMG.ru_div(img.data,cc,q);
-imwrite(uimg,['/home/jbpritts/Documents/accv18_rebuttal/img/' ...
-         'ud_H4ql.jpg']);
-
-%figure;
-%image([80 475], [200 875], cropped/bright);
-load('output/cropped_dartboard_H222ql.mat'); 
-%hold on;
-%for k = 1:numel(stats_list.ransac(end).res.mss)
-%    LAF.draw(gca, x(:,stats_list.ransac(end).res.mss{k}), ...
-%             'Color','w', 'LineWidth',5);
-%    LAF.draw(gca, x(:,stats_list.ransac(end).res.mss{k}), ...
-%             'Color',colors{k},'LineWidth',3);
-%end
-%axis equal;
-%axis tight;
-%axis off;
-%hold off;
-%export_fig( ['/home/jbpritts/Documents/accv18_rebuttal/fig/' ...
-%             'H222ql.png']);
-%
-
-ind = find(~isnan(model_list(1).Gs));
-v = reshape(x(:,ind),3,[]);
-H = model_list(1).A;
-H(3,:) = transpose(model_list(1).l);
-cc = model_list(1).cc;
-q = model_list(1).q;
-
-boundary = IMG.rect_to_boundary(rect);
-[rimg,trect,tform] = IMG.ru_div_rectify(img.data,H,cc,q, ...
-                                        'cspond', v, ...
-                                        'boundary',boundary, ...
-                                        'Registration','Similarity', ...
-                                        'Dims',dims);
-
-image([trect(1) trect(2)],[trect(3) trect(4)],rimg);
-axis off;
-axis equal;
-axis ij;
-y = LAF.warp_fwd(x(:,stats_list.ransac(end).res.mss{1}), ...
-                 tform);
-hold on;
-for k = 1:numel(stats_list.ransac(end).res.mss)
-    LAF.draw(gca, x(:,stats_list.ransac(end).res.mss{k}), ...
-             'Color','w', 'LineWidth',5);
-    LAF.draw(gca, x(:,stats_list.ransac(end).res.mss{k}), ...
-             'Color',colors{k},'LineWidth',3);
-end
-
-hold off;
+export_fig('/home/jbpritts/Documents/accv18/fig/H4ql.png');
 
 
-%imwrite(rimg,['/home/jbpritts/Documents/accv18_rebuttal/img/' ...
-%         'rect_H222ql.jpg']);
-%[uimg,~,trect] = IMG.ru_div(img.data,cc,q);
-%imwrite(uimg,['/home/jbpritts/Documents/accv18_rebuttal/img/' ...
-%         'ud_H222ql.jpg']);
-%
-
-figure;
-image([80 475], [200 875], cropped/bright);
-load('output/cropped_dartboard_H32ql.mat');
-hold on;
-for k = 1:numel(stats_list.ransac(end).res.mss)
-    LAF.draw(gca, x(:,stats_list.ransac(end).res.mss{k}), ...
-             'Color','w', 'LineWidth',5);
-    LAF.draw(gca, x(:,stats_list.ransac(end).res.mss{k}), ...
-             'Color',colors{k},'LineWidth',3);
-end
-axis equal;
-axis tight;
-axis off;
-hold off;
-
-export_fig( ['/home/jbpritts/Documents/accv18_rebuttal/fig/' ...
-             'H32ql.jpg']);
+[ny,nx,~] = size(img.data);
+dims = [ny nx];
 
 ind = find(~isnan(model_list(1).Gs));
 v = reshape(x(:,ind),3,[]);
@@ -157,30 +67,28 @@ image([trect(1) trect(2)],[trect(3) trect(4)],rimg);
 axis off;
 axis equal;
 axis ij;
-y = LAF.warp_fwd(x(:,stats_list.ransac(end).res.mss{1}), ...
-                 tform);
+
 hold on;
 for k = 1:numel(stats_list.ransac(end).res.mss)
-    LAF.draw(gca, x(:,stats_list.ransac(end).res.mss{k}), ...
-             'Color','w', 'LineWidth',5);
-    LAF.draw(gca, x(:,stats_list.ransac(end).res.mss{k}), ...
-             'Color',colors{k},'LineWidth',3);
+    y = LAF.warp_fwd(x(:,stats_list.ransac(end).res.mss{k}), tform);
+    LAF.draw(gca,y,'Color','w', 'LineWidth',5);
+    LAF.draw(gca, y, 'Color',colors{k},'LineWidth',3);
 end
-
 hold off;
 
 figure;
-[uimg,~,trect] = IMG.ru_div(img.data,cc,q);
-
+[uimg,trect,tform] = IMG.ru_div(img.data,cc,q);
+image([trect(1) trect(2)],[trect(3) trect(4)],uimg);
+axis equal;
+axis ij;
+axis off;
 y = LAF.warp_fwd(x(:,stats_list.ransac(end).res.mss{1}), ...
                  tform);
 hold on;
 for k = 1:numel(stats_list.ransac(end).res.mss)
-    LAF.draw(gca, x(:,stats_list.ransac(end).res.mss{k}), ...
-             'Color','w', 'LineWidth',5);
-    LAF.draw(gca, x(:,stats_list.ransac(end).res.mss{k}), ...
-             'Color',colors{k},'LineWidth',3);
+    y = LAF.warp_fwd(x(:,stats_list.ransac(end).res.mss{k}), tform);
+    LAF.draw(gca, y, 'Color','w', 'LineWidth',5);
+    LAF.draw(gca, y, 'Color',colors{k},'LineWidth',3);
 end
 
 hold off;
-
