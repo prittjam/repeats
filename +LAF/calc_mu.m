2747e6a314e9115bb47cac81ba2560e0aeb33ddb
jbpritts <jbpritts@gmail.com>
2018-10-31 16:52:16 +0100

    changes

--- +LAF/calc_mu.m
+++
@@ -1,8 +0,0 @@
-%
-%  Copyright (c) 2018 James Pritts
-%  Licensed under the MIT License (see LICENSE for details)
-%
-%  Written by James Pritts
-%
-function mux = calc_mu(x)
-mux = [(x(1:2,:)+x(4:5,:)+x(7:8,:))/3; ones(1,size(x,2))];
