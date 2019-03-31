35667c7595433ac09f2adbe8763f0da5d8b8ea35
James Pritts <jbpritts@gmail.com>
2019-03-31 13:22:33 -0400

    changes

---
+++ cvpr14/solvers/+WRAP/laf22_to_ql.m
@@ -0,0 +1,122 @@
+% Copyright (c) 2017 James Pritts
+% 
+classdef laf22_to_ql < WRAP.RectSolver
+    properties
+        q = [];
+        solver_type = 'polynomial';
+        qvals = [0:-1:-6];
+    end
+
+    methods(Static)
+        function H = solve_accv10(aX,arsc)
+            if ~iscell(aX)
+                aX = {aX};
+                arsc = {arsc};
+            end
+
+            ALLX = [aX{:}];
+            ALLX = ALLX(1:2,:);
+
+            tx = mean(ALLX(1,:));
+            ty = mean(ALLX(2,:));
+            ALLX(1,:) = ALLX(1,:) - tx;
+            ALLX(2,:) = ALLX(2,:) - ty;
+            dsc = max(abs(ALLX(:)));
+
+            A = eye(3);
+            A([1,2],3) = -[tx ty] / dsc;
+            A(1,1) = 1 / dsc;
+            A(2,2) = 1 / dsc;
+
+            len = length(aX);
+            Z = [];
+            R = [];
+
+            for i = 1:len
+                X = aX{i};
+                rsc = arsc{i};
+                X(1,:) = (X(1,:) - tx) / dsc;
+                X(2,:) = (X(2,:) - ty) / dsc;
+                
+                z = [rsc .* X(1,:); rsc .* X(2,:)];
+                z(len+2, :) = 0;
+                z(i+2,:) = -ones(1, size(X,2));
+                
+                Z = [Z; z'];
+                R = [R; rsc(:)];
+            end
+
+            hs = pinv(Z) * -R;
+
+            H = eye(3);
+
+            H(3,1) = hs(1);
+            H(3,2) = hs(2);
+            H = H * A;
+        end
+        
+        function [H,solver_time] = iter_solve_accv10(u,G)
+            v = u;
+            H = eye(3,3);
+            solver_time = 0;
+            for k = 1:1         
+                [mu,sc] = ...
+                    cmp_splitapply(@(x) ...
+                                   (deal({(x(1:2,:)+x(4:5,:)+x(7:8,:))/3}, ...
+                                         {1./ ...
+                                    nthroot(PT.calc_scale(x),3)})),v,G);
+                tic;
+                Hk = WRAP.laf22_to_ql.solve_accv10(mu,sc);
+                tmp_solver_time = toc;
+                H = Hk*H;
+                v = PT.renormI(blkdiag(H,H,H)*u);
+                solver_time = solver_time+tmp_solver_time;
+            end
+        end        
+    end
+    
+    methods
+        function this = laf22_to_ql(varargin)
+            this = this@WRAP.RectSolver('22');
+            this = cmp_argparse(this,varargin{:});
+        end
+
+        function M = fit(this,x,idx,cc,varargin)
+            qn = this.qvals(randi(numel(this.qvals)));
+            q = qn/sum(2*cc)^2;
+            xu = PT.ru_div(x,cc,q);
+            switch(this.solver_type)
+              case 'polynomial'
+                M = [];
+                A = [1 0 -cc(1); ...
+                     0 1 -cc(2); ...
+                     0 0  1];    
+                x2 = A*reshape(xu(:,[idx{:}]),3,[]);
+                tic
+                [l,sols] = ...
+                    solver_changeofscale_22_det_norad(x2(1:2,1:3), ...
+                                                      x2(1:2,4:6), ...
+                                                      x2(1:2,7:9), ...
+                                                      x2(1:2,10:12));
+                solver_time = toc;
+                N = size(l,2);
+                if N > 0 
+                    ll2 = A'*l;
+                    ll2 = bsxfun(@rdivide,ll2,ll2(3,:));
+                    q = repmat(q,1,N);
+                    M = struct('l', mat2cell(real(ll2),3,ones(1,N)), ...
+                               'q', mat2cell(q,1,ones(1,N)), ...
+                               'solver_time', solver_time);
+                end
+                
+              case 'linear'
+                G = varargin{1};
+                [Hinf,solver_time] = ...
+                    this.iter_solve_accv10(xu(:,[idx{:}]),findgroups(G([idx{:}])));
+                M = struct('l', transpose(Hinf(3,:)), ...
+                           'q', q, ... 
+                           'solver_time', solver_time);
+            end
+        end
+    end
+end
