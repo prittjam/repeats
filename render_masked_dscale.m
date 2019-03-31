3879926246f6612ff36ea965578b62fa5787df80
James Pritts <jbpritts@gmail.com>
2019-03-31 13:50:16 -0400

    changes

--- render_masked_dscale.m
+++ render_masked_dscale.m
@@ -1,4 +1,6 @@
 function [] = render_masked_dscale(results_file,mask)
+results_file = '/home/jbpritts/src/repeats/results/WRAP.laf222_to_ql/20190331_111605/cropped_dartboard.mat';
+mask = imread('/home/jbpritts/src/repeats/results/WRAP.laf222_to_ql/20190331_111605/cropped_dartboard_mask.png');
 load(results_file);
 res = res_list(1);
 l = PT.renormI(model_list(1).l);
@@ -8,9 +10,13 @@ x = reshape(meas.x(:,unique(res.info.cspond(:,res.cs))),3,[]);
                     model_list(1).cc,model_list(1).q,x);
 sc_img = 1./sc_img;
 bwmask = mat2gray(mask);
-mask(mask < 0.95) = 0;
+bgimg = 0.5*img.data;
 bwmask(bwmask == 0) = nan;
 sc_image = bwmask.*sc_img;
-scimg8 = ind2rgb(im2uint8(mat2gray(sc_image)),parula(256));
+scimg8 = im2uint8(ind2rgb(im2uint8(mat2gray(sc_image)),parula(256)));
+bwmask = repmat(bwmask,1,1,3);
+bgimg(bwmask==1) = scimg8(bwmask==1);
 [~,img_name] = fileparts(results_file);
-imwrite(scimg8,[img_name '_cs.jpg']);
+imwrite(bgimg,[img_name '_cs.jpg']);
+
+
