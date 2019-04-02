9a89fa6192829e1c45f5b764b710728785b39b16
James Pritts <jbpritts@gmail.com>
2019-04-01 22:20:02 -0400

    changes

---
+++ save_imgs.m
@@ -0,0 +1,15 @@
+function [] = save_imgs(results_path,img_path,uimg,rimg,sc_img);
+if ~exist(results_path, 'dir')
+    mkdir(results_path);
+end
+    
+[~,img_name] = fileparts(img_path);
+
+ud_file_path = fullfile(results_path,[img_name '_ud.jpg']);
+imwrite(uimg,ud_file_path);
+
+rect_file_path = fullfile(results_path,[img_name '_rect.jpg']);
+imwrite(rimg,rect_file_path);
+
+sc_file_path = fullfile(results_path,[img_name '_cs.jpg']);
+imwrite(sc_img,sc_file_path);
