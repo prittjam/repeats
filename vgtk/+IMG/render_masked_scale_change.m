a1ded876ceb56d7890ddf9a44699020bf982637a
James Pritts <jbpritts@gmail.com>
2019-04-01 22:43:16 -0400

    changes

--- vgtk/+IMG/render_masked_scale_change.m
+++ vgtk/+IMG/render_masked_scale_change.m
@@ -1,17 +1,15 @@
-function [] = render_masked_scale_change(img,meas,model,res,mask)
+function bgimg = render_masked_scale_change(img,meas,model,res,mask)
 l = PT.renormI(model_list(1).l);
 x = reshape(meas.x(:,unique(res.info.cspond(:,res.cs))),3,[]);
 [sc_img,si_fn] = ...
-    IMG.render_scale_change(size(img.data),model_list(1).l, ...
+    IMG.render_scale_change(size(img),model_list(1).l, ...
                             model_list(1).cc,model_list(1).q,x);
 sc_img = 1./sc_img;
 bwmask = mat2gray(mask);
-bgimg = img.data;
+bgimg = img;
 bwmask(bwmask == 0) = nan;
 sc_image = bwmask.*sc_img;
 keyboard;
 scimg8 = im2uint8(ind2rgb(im2uint8(mat2gray(sc_image)),parula(256)));
 bwmask = repmat(bwmask,1,1,3);
 bgimg(bwmask==1) = scimg8(bwmask==1);
-[~,img_name] = fileparts(results_file);
-imwrite(bgimg,[img_name '_cs.jpg']);
