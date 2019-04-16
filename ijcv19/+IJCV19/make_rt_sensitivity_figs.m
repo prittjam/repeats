57aeb15ea8e356fd3c19fcc9f157fc08fb09d75b
James Pritts <jbpritts@gmail.com>
2019-04-16 16:06:43 -0400

    chagnes

---
+++ ijcv19/+IJCV19/make_rt_sensitivity_figs.m
@@ -0,0 +1,10 @@
+function [] = make_rt_sensitivity_figs()
+[solver_names,solver_list,colormap] = IJCV19.make_solver_list();
+
+target_path = '/home/jbpritts/Documents/ijcv19/fig2/';
+rt_wildcard = 'results/rt_sensitivity*.mat';
+rt_listing = dir(rt_wildcard);
+rt_file_path = [rt_listing(end).folder '/' rt_listing(end).name];
+
+keyboard;
+TEST.make_sensitivity_figs(rt_file_path,target_path,colormap, 'rt');
