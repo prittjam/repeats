eb0e42cffede329a49def053f5e1988799892212
James Pritts <jbpritts@gmail.com>
2019-09-10 01:28:14 -0400

    changes

--- scene_sim/+TEST/make_sensitivity_figs.m
+++ scene_sim/+TEST/make_sensitivity_figs.m
@@ -139,7 +139,6 @@ drawnow;
 cleanfigure;
 matlab2tikz([target_path sprintf('ransac_xfer_sensitivity_%s.tikz',rigid_xform)], ...
             'width','\fwidth','extraAxisOptions',axis_options);
-keyboard;
 
 function boxplot_colors = make_boxplot_colors(res,colormap)
 num_solvers = numel(unique(res.solver));
